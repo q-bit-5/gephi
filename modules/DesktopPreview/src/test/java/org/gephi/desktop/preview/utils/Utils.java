9a0389787839a7ebfe54f447106b86891786fe3e
Mathieu Bastian <mathieu.bastian@gmail.com>
2026-04-11 16:33:23 +0200

    Fix Preview utils

--- modules/DesktopPreview/src/test/java/org/gephi/desktop/preview/utils/Utils.java
+++ modules/DesktopPreview/src/test/java/org/gephi/desktop/preview/utils/Utils.java
@@ -3,6 +3,7 @@ package org.gephi.desktop.preview.utils;
 import org.gephi.desktop.preview.PreviewUIModelImpl;
 import org.gephi.preview.api.PreviewController;
 import org.gephi.preview.api.PreviewModel;
+import org.gephi.preview.api.PreviewPreset;
 import org.gephi.project.impl.WorkspaceImpl;
 import org.openide.util.Lookup;
 
@@ -12,7 +13,7 @@ public class Utils {
         WorkspaceImpl workspace = new WorkspaceImpl(null, 0);
         PreviewController previewController = Lookup.getDefault().lookup(PreviewController.class);
         PreviewModel previewModel = previewController.getModel(workspace);
-        PreviewUIModelImpl model = new PreviewUIModelImpl(previewModel);
+        PreviewUIModelImpl model = new PreviewUIModelImpl(previewModel, new PreviewPreset[0], new PreviewPreset[0]);
         workspace.add(model);
         return model;
     }
